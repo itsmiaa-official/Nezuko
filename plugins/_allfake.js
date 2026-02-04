528f3cac5cd3a23c75cb1870a192c49c97b4a2ef
𝐌𝐢𝐚 🍒 <hibqbqxdd@gmail.com>
2026-02-04 01:14:55 -0300

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -17,7 +17,7 @@ handler.all = async function (m, { conn }) {
         isForwarded: true,
         forwardedNewsletterMessageInfo: { newsletterJid: idcanal, serverMessageId: 100, newsletterName: nombrecanal },
         externalAdReply: {
-          title: '${global.botname}',
+          title: '𝐒𝐩𝐚𝐜𝐞',
           thumbnailUrl: 'https://chat.whatsapp.com/KAhwtBdTOYlFsbsU8rwo79',
           thumbnail: await (await fetch(global.iconorcanal)).buffer(),
           mediaType: 1,
