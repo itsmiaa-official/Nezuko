6b3573357637edf5fd05c8b252e4a0e30bcf4160
𝐌𝐢𝐚 🍒 <hibqbqxdd@gmail.com>
2026-02-03 22:53:22 -0300

    Crear tools-toimg.js

---
+++ plugins/tools-toimg.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+if (!m.quoted) {
+return conn.reply(m.chat, `ꕤ Debes citar un sticker para convertir a imagen.`, m)
+}
+await m.react('🕒')
+let xx = m.quoted
+let imgBuffer = await xx.download()   
+if (!imgBuffer) {
+await m.react('❌')
+return conn.reply(m.chat, `ꕥ No se pudo descargar el sticker.`, m)
+}
+await conn.sendMessage(m.chat, { image: imgBuffer, caption: '> *Aquí tienes ฅ^•ﻌ•^ฅ*' }, { quoted: m })
+await m.react('✅')
+}
+
+handler.help = ['toimg']
+handler.tags = ['tools']
+handler.command = ['toimg', 'jpg', 'img'] 
+handler.register = true
+
+export default handler
