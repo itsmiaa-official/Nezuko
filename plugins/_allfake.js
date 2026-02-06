d8deab898435a9cb942b5db2df435497f68f7f91
𝐌𝐢𝐚 🍒 <hibqbqxdd@gmail.com>
2026-02-05 23:35:15 -0300

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -7,7 +7,7 @@ let handler = m => m
 handler.all = async function (m, { conn }) {
 
     global.logo = 'https://raw.githubusercontent.com/El-brayan502/img/upload/uploads/e97fef-1769474597244.jpg' 
-    global.iconorcanal = 'https://raw.githubusercontent.com/El-brayan502/img/upload/uploads/e97fef-1769474597244.jpg'
+    global.iconorcanal = 'https://files.catbox.moe/3uzi5u.jpg'
 
     global.idcanal = "120363345778623279@newsletter" 
     global.nombrecanal = "=͟͟͞͞𝐒𝐩𝐚𝐜𝐞 𝐖𝐨𝐫𝐥𝐝 𝐂𝐥𝐮𝐛'𝐬 - 𝐎𝐟𝐟𝐢𝐜𝐢𝐚𝐥 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 ✰"
