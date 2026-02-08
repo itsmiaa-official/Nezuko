be156179cf324a4e80c6b275e7460106cf414cfe
𝐌𝐢𝐚 🍒 <hibqbqxdd@gmail.com>
2026-02-07 23:35:08 -0300

    Actualizar main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -53,7 +53,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 *ㅤꨶ〆⁾ ㅤׄㅤ⸼ㅤׄ *͜⚡* ㅤ֢ㅤ⸱ㅤᯭִ*
 ㅤ𓏸𓈒ㅤׄ *sᴏʏ* :: *${botName.toUpperCase()}*
-ׅㅤ𓏸𓈒ㅤׄ *ᴛʏᴘᴇ* :: *${isSubBot ? '𝗦𝘂𝗯-𝗕𝗼𝘁 🅑' : '𝗣𝗿𝗶𝗻𝗰𝗶𝗽𝗮𝗹 🅥'}
+ׅㅤ𓏸𓈒ㅤׄ *ᴛʏᴘᴇ* :: *${isSubBot ? '𝗦𝘂𝗯-𝗕𝗼𝘁 🅑' : '𝗣𝗿𝗶𝗻𝗰𝗶𝗽𝗮𝗹 🅥'}*
 ׅㅤ𓏸𓈒ㅤׄ *ᴅᴇᴠᴇʟᴏᴘᴇʀ* :: 𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 👑
 ׅㅤ𓏸𓈒ㅤׄ *ᴠᴇʀsɪᴏɴ* :: ${version}
 ׅㅤ𓏸𓈒ㅤׄ *sᴇʀᴠɪᴅᴏʀ* :: México 🇲🇽 
@@ -61,12 +61,12 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 > ## \`𝖨𝖭𝖥𝖮𝖱𝖬𝖠𝖢𝖨𝖮́𝖭 ⚔️\`
 
-ׅㅤ𓏸𓈒ㅤׄ *ᴘʀᴇғɪᴊᴏ* \`::\` ${botPrefix} 
-ׅㅤ𓏸𓈒ㅤׄ *ᴄᴏᴍᴀɴᴅᴏs* \`::\` ${totalCommands}   
-ׅㅤ𓏸𓈒ㅤׄ *ᴍᴏᴅᴏ* \`::\` ${botMode === 'private' ? '𝗣𝗿𝗶𝘃𝗮𝗱𝗼' : '𝗣𝘂́𝗯𝗹𝗶𝗰𝗼'}
-ׅㅤ𓏸𓈒ㅤׄ *ᴜsᴜᴀʀɪᴏs* \`::\` ${totalUsers.toLocaleString()}
+ׅㅤ𓏸𓈒ㅤׄ *ᴘʀᴇғɪᴊᴏ* :: ${botPrefix} 
+ׅㅤ𓏸𓈒ㅤׄ *ᴄᴏᴍᴀɴᴅᴏs* :: ${totalCommands}   
+ׅㅤ𓏸𓈒ㅤׄ *ᴍᴏᴅᴏ* :: ${botMode === 'private' ? '𝗣𝗿𝗶𝘃𝗮𝗱𝗼' : '𝗣𝘂́𝗯𝗹𝗶𝗰𝗼'}
+ׅㅤ𓏸𓈒ㅤׄ *ᴜsᴜᴀʀɪᴏs* :: ${totalUsers.toLocaleString()}
 ㅤ𓏸𓈒ㅤׄ *ᴘɪɴɢ* :: ${Date.now() - m.timestamp}ms
-ׅㅤ𓏸𓈒ㅤׄ *ʟɪʙʀᴇʀɪᴀ* \`::\` ${global.libreria || 'Baileys Multi Device'} 
+ׅㅤ𓏸𓈒ㅤׄ *ʟɪʙʀᴇʀɪᴀ* :: ${global.libreria || 'Baileys Multi Device'} 
 
 
 📌 *Seleccionacciona una categoría:*`
